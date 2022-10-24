b22e9ff50bd5ca2e801d41455fc6643aafa93cd8
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-10-24 13:44:47 -0400

    add searchbar and delete extra images

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -7,5 +7,17 @@ let wind = document.getElementById("wind");
 let visibility = document.getElementById("visibility");
 
 let menuBars = document.querySelector(".menu-bars");
-let searchBar = document.querySelector("search-bar");
+let searchBar = document.getElementById("searchbar");
 
+let search_label = document.querySelector(".search-label");
+let search_input = document.querySelector("#search-input");
+
+search_label.addEventListener("click", function (){
+    searchBar.classList.toggle("collapsed");
+})
+
+search_input.addEventListener("keydown", function(e){
+    if(e.key === "enter"){
+       console.log("Click!")
+    }
+})
