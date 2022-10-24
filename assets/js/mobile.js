17caa755d35db374283efb853cc3575c032c4de4
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-10-23 21:53:20 -0400

    add div to contain searched cities

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -6,5 +6,17 @@ let humidity = document.getElementById("humidity");
 let wind = document.getElementById("wind");
 let visibility = document.getElementById("visibility");
 
-let menuBars = document.querySelector("menu-bars");
+let menuBars = document.querySelector(".menu-bars");
 let searchBar = document.querySelector("search-bar");
+
+let nav = document.querySelector(".nav");
+
+function openNav(){
+    nav.style.width = "250px";
+};
+
+function closeNav (){
+    nav.style.width = "0px";
+};
+
+menuBars.addEventListener("click", openNav);
