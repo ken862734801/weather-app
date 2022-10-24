945fb2e35ea0bc6198873592f81817a9af07786c
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-10-23 21:31:51 -0400

    define DOM variables

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,8 +1,10 @@
 // Variables for the DOM elements.
-let city = document.querySelector("city");
-let temperature;
-let feels_like;
-let humidity;
-let wind;
-let visibility;
+let city = document.getElementById("city");
+let temperature = document.getElementById("temperature");
+let feels_like = document.getElementById("feels-like");
+let humidity = document.getElementById("humidity"); 
+let wind = document.getElementById("wind");
+let visibility = document.getElementById("visibility");
 
+let menuBars = document.querySelector("menu-bars");
+let searchBar = document.querySelector("search-bar");
